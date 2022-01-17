f3bfaa1569daba53f72d1c3806dd434369194f4d
liming <1018457683@qq.com>
2022-01-17 17:47:55 +0800

    TS复习到接口

--- 08-TypeScript/00-TypeScript官方文档/02-接口/06-接口描述函数类型.ts
+++ 08-TypeScript/00-TypeScript官方文档/02-接口/06-接口描述函数类型.ts
@@ -5,10 +5,16 @@
  * @FilePath: \02-手册指南\02-接口\06-函数类型.ts
  */
 interface SearchFunc {
-  (source:string,subString:string):boolean;
+    (source: string, subString: string): boolean;
+    //约束这个函数的参数类型和返回值类型
 }
 
 // 这样定义后，我们可以像使用其它接口一样使用这个函数类型的接口。
+
+/**
+ * 为了使用接口表示函数类型，我们需要给接口定义一个调用签名。
+ *  它就像是一个只有参数列表和返回值类型的函数定义。参数列表里的每个参数都需要名字和类型。
+ */
 
 let mySearch: SearchFunc;
 mySearch = function (source: string, subString: string): boolean {
@@ -19,3 +25,12 @@ mySearch = function (source: string, subString: string): boolean {
 /**
  * 对于函数类型的类型检查来说，函数的参数名不需要与接口里定义的名字相匹配。 比如，我们使用下面的代码重写上面的例子：
  */
+
+let mySearch2: SearchFunc;
+mySearch2 = function(src: string, sub: string): boolean {
+  let result = src.search(sub);
+  return result > -1;
+}
+
+console.log(mySearch('你好','不'));
+
